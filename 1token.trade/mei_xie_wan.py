cae8e8ec6eed6214b8cd1c84cddc81909c0bf23d
mj3428 <38501129+mj3428@users.noreply.github.com>
2018-06-04 09:47:55 +0800

    Update mei_xie_wan.py

--- 1token.trade/mei_xie_wan.py
+++ 1token.trade/mei_xie_wan.py
@@ -1,7 +1,7 @@
 %matplotlib inline
 import numpy as np
 import statsmodels.api as sm
-from pandas.stats.api import ols
+import pandas.stats.api as ols
 from pandas import DataFrame
 import OnePy as op
 from OnePy.builtin_module.recorders.stock_recorder import StockRecorder
@@ -39,10 +39,14 @@ class SmaStrategy(op.StrategyBase):
 
     def __init__(self):
         super().__init__()
-    def initial():
+    def initial(slef):
         global N,M,ans,ans_rightdev
+        
         N=18
         M=300
+        
+        main(self)
+        
         prices=op.data_readers.CSVReader('e:/binancedata/merge2.csv', 'merge2',fromdate='2017-08-18 08:00:00', todate='2018-05-12 14:00:00')
         highs=prices.high
         lows=prices.low
@@ -57,57 +61,54 @@ class SmaStrategy(op.StrategyBase):
             #计算r2
             ans_rightdev.append(results.rsquared)
         
-'''def run_time():
-    for h in range(0,24):
-        for m in range(0,60,15):
-            if m<=9:
-                m=('0'+str(m))
-                times.append(str(h)+':'+str(m))
-    for item in times[:]:
-        if item=='24:00':
-            times.remove(item)
-    return times'''
+    '''def run_time():
+        for h in range(0,24):
+            for m in range(0,60,15):
+                if m<=9:
+                    m=('0'+str(m))
+                    times.append(str(h)+':'+str(m))
+        for item in times[:]:
+            if item=='24:00':
+                times.remove(item)
+        return times'''
 
-def main():
-    global init,ans_rightdev,ans,M,buy,sell
-    SS=SmaStrategy()
-    beta=0
-    r2=0
-    if init:
-        init=False
-    else:
-        a=attribute(10)
-        highs=a.calculatehighs
-        lows=a.calculatelows
-        X = sm.add_constant(lows)
-        model = sm.OLS(highs, X)
-        beta = model.fit().params[1]
-        ans.append(beta)
-        #计算r2
-        r2=model.fit().rsquared
-        ans_rightdev.append(r2)
-    section =ans[-M:]
-    # 计算均值序列
-    mu = np.mean(section)
-    # 计算标准化RSRS指标序列
-    sigma = np.std(section)
-    zscore = (section[-1]-mu)/sigma
-    #计算右偏RSRS标准分
-    zscore_rightdev= zscore*beta*r2
-    
-    if zscore_rightdev > buy:
-        order_value(security, cash)
-    # 如果上一时间点的RSRS斜率小于卖出阈值, 则空仓卖出
-    elif zscore_rightdev < sell:
+    def main(self):
+        global init,ans_rightdev,ans,M,buy,sell
         
+        beta=0
+        r2=0
+        if init:
+            init=False
+        else:
+            a=attribute(50)
+            highs=a.calculatehighs
+            lows=a.calculatelows
+            X = sm.add_constant(lows)
+            model = sm.OLS(highs, X)
+            beta = model.fit().params[1]
+            ans.append(beta)
+            #计算r2
+            r2=model.fit().rsquared
+            ans_rightdev.append(r2)
+        section =ans[-M:]
+        # 计算均值序列
+        mu = np.mean(section)
+        # 计算标准化RSRS指标序列
+        sigma = np.std(section)
+        zscore = (section[-1]-mu)/sigma
+        #计算右偏RSRS标准分
+        zscore_rightdev= zscore*beta*r2
     
-
-
+        if zscore_rightdev > buy:
+            self.buy(0.5, 'merge2', takeprofit=50,stoploss=10, trailingstop_pct=1)
+        # 如果上一时间点的RSRS斜率小于卖出阈值, 则空仓卖出
+        elif zscore_rightdev < sell:
+            self.sell(0.5, 'merge2', takeprofit=50,stoploss=10, trailingstop_pct=1)
+        
 
 #op.data_readers.CSVReader('e:/binancedata/merge2.csv', 'merge2',fromdate='2017-08-19', todate='2018-01-01')
 
-
-#SmaStrategy()
+SmaStrategy()
 
 op.RiskManagerBase()
 op.StockBroker()
