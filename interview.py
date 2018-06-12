6d441d7a3bd64394257079fcccf0c3b90b27287c
mj3428 <38501129+mj3428@users.noreply.github.com>
2018-06-12 16:15:25 +0800

    Update interview.py

--- interview.py
+++ interview.py
@@ -12,5 +12,11 @@ print([i for i in range(4)])
 print ([m(2) for m in num()]) #[6, 6, 6, 6]'''
 
 '''print("\n".join("\t".join(["%s*%s=%s" %(x,y,x*y) for y in range(1, x+1)]) for x in range(1, 10)))
-#一行代码打印九九乘法表'''
+#一行代码打印九九乘法表
+print([ i % 2 for i in range(10) ]) #[0, 1, 0, 1, 0, 1, 0, 1, 0, 1]
+print( i % 2 for i in range(10) ) #<generator object <genexpr> at 0x000000000273CCA8>
 
+python里面的坑: 函数的默认参数是一个list 
+当第一次执行的时候实例化了一个list 
+第二次执行还是用第一次执行的时候实例化的地址存储 
+所以三次执行的结果就是 [1, 1, 1] 想每次执行只输出[1] ，默认参数应该设置为None'''
