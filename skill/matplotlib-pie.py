d65493a138a824c17c38b44bc20a910308d31c22
mj3428 <38501129+mj3428@users.noreply.github.com>
2018-06-19 11:20:38 +0800

    Update matplotlib-pie.py

--- skill/matplotlib-pie.py
+++ skill/matplotlib-pie.py
@@ -1,1 +1,7 @@
-
+from matplotlib import font_manager as fm
+import matplotlib as mpl
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+% matplotlib inline
+plt.style.use('ggplot')
