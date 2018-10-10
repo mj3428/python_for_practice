1b9a3d37ee9d79db4ca77dbd2745fc8bf7ef9f9f
mj3428 <38501129+mj3428@users.noreply.github.com>
2018-10-10 20:18:21 +0800

    Update and rename Login.py to crawler.py

--- skill/Login.py
+++
@@ -1,1 +0,0 @@
-https://github.com/Python3WebSpider/GithubLogin
---
+++ skill/crawler.py
@@ -0,0 +1,2 @@
+https://github.com/Python3WebSpider/GithubLogin #模拟登录
+https://blog.csdn.net/qq_21180877/article/details/79137699 #实战之解析
