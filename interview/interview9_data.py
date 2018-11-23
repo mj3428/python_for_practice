b09922e9c9cced483a8dab6f4e6f49c7b715eea0
mj3428 <38501129+mj3428@users.noreply.github.com>
2018-11-23 15:51:15 +0800

    Update interview9_data.py

--- interview/interview9_data.py
+++ interview/interview9_data.py
@@ -1,1 +1,48 @@
+'''
+MySQL索引种类
+MySQL目前主要有以下几种索引类型：
+1.普通索引
+2.唯一索引
+3.主键索引
+4.组合索引
+5.全文索引
+CREATE TABLE table_name[col_name data type]
+[unique|fulltext][index|key][index_name](col_name[length])[asc|desc]
+1.unique|fulltext为可选参数，分别表示唯一索引、全文索引
+2.index和key为同义词，两者作用相同，用来指定创建索引
+3.col_name为需要创建索引的字段列，该列必须从数据表中该定义的多个列中选择
+4.index_name指定索引的名称，为可选参数，如果不指定，默认col_name为索引值
+5.length为可选参数，表示索引的长度，只有字符串类型的字段才能指定索引长度
+6.asc或desc指定升序或降序的索引值存储
+1.普通索引
+（1）直接创建索引 CREATE INDEX index_name ON table(column(length)
+2.唯一索引
+与前面的普通索引类似，不同的就是：索引列的值必须唯一，但允许有空值。如果是组合索引，则列值的组合必须唯一
+（1）创建唯一索引 CREATE UNIQUE INDEX indexName ON table(column(length))
+3.主键索引
+是一种特殊的唯一索引，一个表只能有一个主键，不允许有空值。一般是在建表的时候同时创建主键索引：
+CREATE TABLE `table` (
+    `id` int(11) NOT NULL AUTO_INCREMENT ,
+    `title` char(255) NOT NULL ,
+    PRIMARY KEY (`id`)
+);
+4.组合索引
+指多个字段上创建的索引，只有在查询条件中使用了创建索引时的第一个字段，索引才会被使用。使用组合索引时遵循最左前缀集合
+ALTER TABLE `table` ADD INDEX name_city_age (name,city,age); 
+5.全文索引
+主要用来查找文本中的关键字，而不是直接与索引中的值相比较。fulltext索引跟其它索引大不相同，它更像是一个搜索引擎，而不是简单的where语句的参数匹配。
+fulltext索引配合match against操作使用，而不是一般的where语句加like。
+它可以在create table，alter table ，create index使用，不过目前只有char、varchar，text 列上可以创建全文索引。
+值得一提的是，在数据量较大时候，现将数据放入一个没有全局索引的表中，
+然后再用CREATE index创建fulltext索引，要比先为一张表建立fulltext然后再将数据写入的速度快很多。
+（1）创建表的适合添加全文索引
+CREATE TABLE `table` (
+    `id` int(11) NOT NULL AUTO_INCREMENT ,
+    `title` char(255) CHARACTER NOT NULL ,
+    `content` text CHARACTER NULL ,
+    `time` int(10) NULL DEFAULT NULL ,
+    PRIMARY KEY (`id`),
+    FULLTEXT (content)
+);
+
 
