65cc923e960b7cb0af53da7f547e6b392dbe78aa
mj3428 <38501129+mj3428@users.noreply.github.com>
2019-01-02 12:30:38 +0800

    Update table_cell.py

--- python-docx/table_cell.py
+++ python-docx/table_cell.py
@@ -1,4 +1,16 @@
+#修改单元格内的段落及字体
+
+#方法1
 cell = table.cell(0, 0)
 cell.text = 'Shift'
 cell_font = cell.text_frame.paragraphs[0].runs[0].font
 cell_font.size = Pt(10)
+
+#方法2
+cells[0].text = 'Some text'   #Write the text to the cell
+#Modify the paragraph alignment, 
+first paragraph cells[0].paragraphs[0].paragraph_format.alignment=WD_ALIGN_PARAGRAPH.CENTER
+#or
+row_cells[0].text = ''
+row_cells[0].paragraphs[0].add_run('Total').bold = True
+row_cells[0].paragraphs[0].paragraph_format.alignment = WD_ALIGN_PARAGRAPH.RIGHT
