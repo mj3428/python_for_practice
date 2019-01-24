9fd27de7652bdc8aca9b7cd2fa9d8f51f792a229
mj3428 <38501129+mj3428@users.noreply.github.com>
2019-01-24 13:45:07 +0800

    Update vote_break.py

--- skill/vote_break.py
+++ skill/vote_break.py
@@ -1,8 +1,31 @@
-queryString = 'rnd=4868944880959973&authType=10&id=448809'
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: miaojue
+@contact: major3428@foxmail.com
+@software: pycharm
+@file: test7.py
+@time: 2019-1-15 上午 8:54
+@desc:
+'''
+
+import random
+import hashlib
+ex = random.randint(40000, 59999)
+
+queryString = 'rnd=48689448809' + str(ex) + '&authType=10&id=448809'
 strs = queryString.split("&")
-print(str)
+print(strs)
 strs.sort()
 vals = ''
 for i in range(len(strs)):
     vals = vals+strs[i].split("=")[1]
 print(vals)
+m2 = hashlib.md5()
+m2.update(str(vals).encode('utf-8'))
+hex_md5 = m2.hexdigest()
+print(ex)
+new_url = 'http://v.10pinping.com/api/captcha.check.2.php' \
+          '?rnd=48689448809' + str(ex) +'&authType=10&id=448809&sign=' + hex_md5
+print(new_url)
+
