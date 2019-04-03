05afc05d282eba99d5073565dabad6b02efe7b44
mj3428 <38501129+mj3428@users.noreply.github.com>
2019-04-03 10:05:36 +0800

    Update knn.py

--- data_analysis/knn.py
+++ data_analysis/knn.py
@@ -26,11 +26,20 @@ plt.show()
 
 # 分割数据，将25%的数据作为测试集，其余作为训练集
 train_x, test_x, train_y, test_y = train_test_split(data, digits.target, test_size=0.25, random_state=33)
+# train_x与train_y都是训练集？
+#  训练集的特征矩阵和分类结果。对应test_x和test_y是测试集的特征矩阵和分类结果。
+
 
 # 采用Z-Score规范化
 ss = preprocessing.StandardScaler()
 train_ss_x = ss.fit_transform(train_x)
 test_ss_x = ss.transform(test_x)
+# 为什么test只需要使用transform就可以了？test_ss_x = ss.transform(test_x）
+# 我在train的时候用到了：train_ss_x = ss.fit_transform(train_x)
+# 实际上：fit_transform是fit和transform两个函数都执行一次。所以ss是进行了fit拟合的。只有在fit拟合之后，才能进行transform
+# 在进行test的时候，我们已经在train的时候fit过了，所以直接transform即可。
+# 另外，如果我们没有fit，直接进行transform会报错，因为需要先fit拟合，才可以进行transform。
+
 
 # 创建KNN分类器
 knn = KNeighborsClassifier()
@@ -49,11 +58,15 @@ mm = preprocessing.MinMaxScaler()
 train_mm_x = mm.fit_transform(train_x)
 test_mm_x = mm.transform(test_x)
 
+
 # 创建Naive Bayes分类器
 mnb = MultinomialNB()
 mnb.fit(train_mm_x, train_y) 
 predict_y = mnb.predict(test_mm_x) 
 print("多项式朴素贝叶斯准确率: %.4lf" % accuracy_score(predict_y, test_y))
+# 多项式朴素贝叶斯实际上是符合多项式分布，不会存在负数。而高斯朴素贝叶斯呈现的是高斯分布，
+# 也就是正态分布，比如均值为0，方差为1的标准正态分布，可以存在负数。
+
 
 # 创建CART决策树分类器
 dtc = DecisionTreeClassifier()
